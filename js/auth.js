a335a5dee0d71aa79974e83fdfcbd37ca127cfe0
Serj Gainovskyi <pandastars535@gmail.com>
2026-02-26 20:43:56 +0200

    added nginx.conf

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,5 @@
 // ===== CONFIG =====
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL = "/api";
 
 // ================= INIT =================
 document.addEventListener("DOMContentLoaded", () => {
